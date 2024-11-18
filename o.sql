4d7b703bea59bc18f03105a59cf5c2c305308ef8
LANE <124515167+laneworldwide@users.noreply.github.com>
2024-11-18 13:44:16 -0500

    Create o.sql

---
+++ o.sql
@@ -0,0 +1,9 @@
+/your-repo
+  index.html                <-- This is the landing page!
+  favicon.ico               <-- Your favicon
+  /static/                  <-- Folder with images and assets
+    start-image.png
+    clickable-image.png
+    clickable-image2.png
+    image1.jpg
+  ...
